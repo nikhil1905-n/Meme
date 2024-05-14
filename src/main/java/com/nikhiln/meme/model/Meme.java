9b4eb0da4728058857c342c0f4b049f4b6663910
nikhil1905-n <nikhil1905.n@gmail.com>
2024-05-15 00:36:04 +0530

    Implement model layer

--- src/main/java/com/nikhiln/meme/model/Meme.java
+++ src/main/java/com/nikhiln/meme/model/Meme.java
@@ -1,5 +1,33 @@
 package com.nikhiln.meme.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import java.time.LocalDateTime;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "memes")
 public class Meme {
     
+    @Id
+    private String id;
+
+    @NonNull
+    private String name;
+
+    @NonNull
+    private String url;
+
+    @NonNull
+    private String caption;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
 }
