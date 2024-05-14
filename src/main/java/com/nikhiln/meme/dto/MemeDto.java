ca68af8ceeae38d1a424abf11756eac69401b798
nikhil1905-n <nikhil1905.n@gmail.com>
2024-05-15 00:36:58 +0530

    Implement dto layer

--- src/main/java/com/nikhiln/meme/dto/MemeDto.java
+++ src/main/java/com/nikhiln/meme/dto/MemeDto.java
@@ -1,5 +1,23 @@
 package com.nikhiln.meme.dto;
 
+import javax.validation.constraints.NotBlank;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class MemeDto {
+
+    @NotBlank(message = "UserName cannot be empty")
+    private String name;
+
+    @NotBlank(message = "URL cannot be empty")
+    private String url;
+
+    @NotBlank(message = "Caption cannot be empty")
+    private String caption;
     
 }
--- src/main/java/com/nikhiln/meme/dto/MemeIdDto.java
+++ src/main/java/com/nikhiln/meme/dto/MemeIdDto.java
@@ -1,5 +1,14 @@
 package com.nikhiln.meme.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class MemeIdDto {
     
+    private String id;
+
 }
